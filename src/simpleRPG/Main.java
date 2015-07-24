257ba2c0c6be0768a03011a5e78dd34d31650f05
anonaipai <anonaipai@gmail.com>
2015-07-24 12:19:48 -0500

    Delete Main.java
    Because the folder doesn't exist

--- src/simpleRPG/Main.java
+++
@@ -1,9 +0,0 @@
-package simpleRPG;
-
-public class Main {
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Engine RPG = new Engine();
-		new Thread(RPG).start();
-	}
-}
